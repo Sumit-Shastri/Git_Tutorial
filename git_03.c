1818d3eb42ce3bf2ac37c9c5cf199c5994c3308c
Sumit-Shastri <shastrisumit25@gmail.com>
2025-09-01 17:57:23 +0530

    My first Multi Lines commit mother fucker !!

--- git_03.c
+++ git_03.c
@@ -3,5 +3,8 @@
 int main()
 {
     printf("Learning git today !!");
+    printf("Learning git today !!");
+    printf("Learning git today !!");
+    printf("Learning git today !!");
     return 0;
 }
