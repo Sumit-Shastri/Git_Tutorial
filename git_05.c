fa89a4b55dc8d2d392e4a90cafb93cd5ec7b85a8
Sumit-Shastri <shastrisumit25@gmail.com>
2025-09-04 17:44:42 +0530

    Addition feature add

---
+++ git_05.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int addition(int no1,int no2)
+{
+    int no3 = no1 + no2;
+    printf("Addition is : %d\n",no3);
+}
+
+
+int main()
+{
+    int a , b;
+    printf("Enter value of a : ");
+    scanf("%d",&a);
+    printf("Enter value of b : ");
+    scanf("%d",&b);
+
+    addition(a,b);
+
+
+    return 0;
+}
